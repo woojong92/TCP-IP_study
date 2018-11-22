4bb47b72f7f75f5b36d32fa563b0d7028f3fb6ab
woojong92 <woojong92@gmail.com>
2018-11-22 11:14:45 +0900

    modified "unblocking"

--- week12/io_test.c
+++ week12/io_test.c
@@ -8,12 +8,16 @@
 int main() {
 	int ret;
 	char *buf = (char*)malloc(sizeof(char)*80);
-	int fd = open("/dev/sr0", O_RDWR); // sr0 : cd-rom 
+	//int fd = open("/dev/sr0", O_RDWR); // sr0 : cd-rom 
+	int fd = open("/dev/sr0", O_RDWR|O_NONBLOCK); //
+	printf("fd: %d\n", fd);
 
 	while(1) {
 		ret = read(fd, buf, 80);
 		if( ret ) {
 			write(STDOUT_FILENO, buf, ret);
+		} else {
+			write(STDOUT_FILENO, "No DATA", 7);
 		}
 		usleep(500000);
 	}
